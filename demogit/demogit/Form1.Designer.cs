640cd142f54e593dec43de1619089de34af6a4d1
chantay <tayphan13579@gmail.com>
2025-02-24 14:24:28 +0700

    commit login button

--- demogit/demogit/Form1.Designer.cs
+++ demogit/demogit/Form1.Designer.cs
@@ -73,8 +73,9 @@
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 2;
-            button1.Text = "button1";
+            button1.Text = "Login";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
